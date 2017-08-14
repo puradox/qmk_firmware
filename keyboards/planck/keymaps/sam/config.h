52a3c15e8e6597039e94046191b257a0cd3cb809
Sam Balana <sambalana247@gmail.com>
2017-08-13 23:15:38 -0700

    Add my personal keymap

---
+++ keyboards/planck/keymaps/sam/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define LSPO_KEY KC_LPRN  // Left space cadet shift - (
+#define RSPC_KEY KC_RPRN  // Right space cadet shift - )
+#define DISABLE_SPACE_CADET_ROLLOVER // Use the opposite shift key to cancel the space cadet state
+
+#endif
