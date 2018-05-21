ff862573b312473af28fe2d9ef566718e7fea0a1
Sam Balana <sambalana247@gmail.com>
2018-05-21 03:12:59 -0700

    Convert back to Colemak
    I really can't make up my mind

--- keyboards/planck/keymaps/sam/config.h
+++ keyboards/planck/keymaps/sam/config.h
@@ -6,7 +6,7 @@
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(PLANCK_SOUND)
     #define DEFAULT_LAYER_SONGS { SONG(MARIO_GAMEOVER), /* QWERTY */ \
-                                  SONG(MARIO_THEME)     /* Workman */ \
+                                  SONG(MARIO_THEME)     /* Colemak */ \
                                 }
 #endif
 
